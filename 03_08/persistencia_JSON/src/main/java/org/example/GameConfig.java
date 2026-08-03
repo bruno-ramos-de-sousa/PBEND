87891f6f132f5416026676e500fb87895bafa07d
Bruno <brunoramosdsousa0@gmail.com>
2026-08-03 11:55:17 -0300

    docs: aula 03_08

---
+++ 03_08/persistencia_JSON/src/main/java/org/example/GameConfig.java
@@ -0,0 +1,50 @@
+public class GameConfig {
+    private String playerName;
+    private int levelDifficulty;
+    private boolean audioEnabled;
+    private String screenResolution;
+
+    public GameConfig() {
+        this.playerName = "Player1";
+        this.levelDifficulty = 2;
+        audioEnabled = true;
+        this.screenResolution = "1920x1080";
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+    public int getLevelDifficulty() {
+        return levelDifficulty;
+    }
+    public void setLevelDifficulty(int levelDifficulty) {
+        this.levelDifficulty = levelDifficulty;
+    }
+    public boolean isAudioEnabled() {
+        return audioEnabled;
+    }
+    public void setAudioEnabled(boolean audioEnabled) {
+        this.audioEnabled = audioEnabled;
+    }
+    public String getScreenResolution() {
+        return screenResolution;
+    }
+    public void setScreenResolution(String screenResolution) {
+        this.screenResolution = screenResolution;
+    }
+
+    @Override
+    public String toString() {
+        String statusAudio = audioEnabled ? "Enabled" : "Disabled";
+
+        return "gameConfig{" +
+                "playerName='" + playerName + '\'' +
+                ", levelDifficulty=" + levelDifficulty +
+                ", audioEnabled=" + statusAudio +
+                ", screenResolution='" + screenResolution + '\'' +
+                '}';
+    }
+}
